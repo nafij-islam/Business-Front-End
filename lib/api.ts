5aa0f42d706a4ea846d5858c40fb3cd6498a98c1
Nafij islam <sahariannafis70@gmail.com>
2026-09-19 08:25:09 +0600

    config: configure production backend api url for vercel

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,11 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
+  process.env.NEXT_PUBLIC_API_URL ||
+  (typeof window !== 'undefined' &&
+  (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+    ? 'http://localhost:5000/api/v1'
+    : 'https://business-back-end-5kc1.vercel.app/api/v1');
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
